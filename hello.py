2114f1e9cab12ab063e47316cd2e73d294db1bf4
shi_ww <shi_ww555@163.com>
2025-07-19 23:39:14 +0800

    [7_19] add print string use python

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 print("Hello, world!!")
 print("try git push after connection built")
 print("this is third modification")
+print('He said "good!"')
---
+++ print_string.py
@@ -0,0 +1,19 @@
+print("Hello" + ' world' + " !")
+print('He said "good!"')
+print("He said : \"Let\'s go!\"")
+
+print("Hello\nworld")
+
+print('''this is the first line
+ 
+this is the second line
+ 
+this is the third line
+
+This is the last line''')
+
+
+
+
+
+
