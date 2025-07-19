0d80baa851ad6365c3070b60df2f43f002ab9f11
shi_ww <shi_ww555@163.com>
2025-07-20 00:30:39 +0800

    [learn] [7/19] arithmatic

---
+++ Arithmatic.py
@@ -0,0 +1,13 @@
+import  math
+print(1+1)
+result = float(math.cos(0.8))
+print(result)
+print("---------------------------------------------------")
+a = 1
+b = 9
+c = 20
+
+delta = b**2 - 4*a*c
+
+print((-b + math.sqrt(delta))/(2*a))
+print((-b - math.sqrt(delta))/(2*a))
