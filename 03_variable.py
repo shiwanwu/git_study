417bde9e2303c82c8eaabf1ebf79f41b2e16f8c7
shi_ww <shi_ww555@163.com>
2025-07-19 23:53:25 +0800

    [learn] [7/19] variables

---
+++ 03_variable.py
@@ -0,0 +1,14 @@
+my_love = "135_9969_5621"
+print(my_love)
+
+print("您好，吃了么? " + "张三 ")
+
+greet = "您好，吃了么? "
+
+print(greet + "张三 ")
+
+greet = "Hello, have you eat? "
+
+print(greet + "Zhang San")
+
+print(greet + "张三 ")
