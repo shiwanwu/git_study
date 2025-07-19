aa3b34fcbc6c7e627a8a474d7a85f50075515172
shi_ww <shi_ww555@163.com>
2025-07-20 01:21:31 +0800

    [learn] [7/19] if_branch

---
+++ if_branch.py
@@ -0,0 +1,11 @@
+mood_index = int(input("请输入您的情绪指数(1-100): "))
+if mood_index >= 90:
+    print("今天心情非常好,男朋友可以愉快的打游戏！")
+elif mood_index >= 80:
+    print("今天心情还好，男朋友也打游戏,但是莫嚣张")
+elif mood_index >= 70:
+    print("今天心情normal，男朋友的打游戏,但是请安静点")
+elif mood_index >= 60:
+    print("今天心情及格，男朋友这游戏就打吧。。")
+else:
+    print("想死就去打游戏")
