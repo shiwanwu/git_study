0bcda0e8eb6c13043e8d702b5021b778eced744a
shi_ww <shi_ww555@163.com>
2025-07-20 00:47:40 +0800

    [learn] [7/19] learn variable type

---
+++ Type.py
@@ -0,0 +1,51 @@
+# 对字符串求长度
+var_str = "Hello World!"
+print(len(var_str))
+
+
+#同过索引获取单个字符
+print(var_str[0])
+print("----------------------------")
+print(var_str[-1])
+print(var_str[-2])
+print(var_str[len(var_str) - 1])
+print("----------------------------")
+
+#bool type
+
+b1 = True
+b2 = False
+print(b1)
+
+print("----------------------------")
+
+# 空值类型
+
+c1 = None
+print(c1)
+
+print("----------------------------")
+
+print(type(c1))
+print(type(b1))
+print(type(var_str))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
